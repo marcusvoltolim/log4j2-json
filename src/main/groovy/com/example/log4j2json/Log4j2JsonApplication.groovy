d16cae4a4c24250b872af788d4615e15f55ff853
Marcus Voltolim <marcus.voltolim@gmail.com>
2022-05-04 13:37:22 -0300

    Initial commit: creating the project.

---
+++ src/main/groovy/com/example/log4j2json/Log4j2JsonApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.log4j2json
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Log4j2JsonApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(Log4j2JsonApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/example/log4j2json/Log4j2JsonApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.log4j2json
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Log4j2JsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
